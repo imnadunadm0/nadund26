74e34860942e309a85c8b483a7da18a1e59c7e2b
nadun adm <imnadunadm@gmail.com>
2025-12-27 22:45:18 +0530

    Fix TypeScript error in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import "./globals.css";
+import { PropsWithChildren } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className="bg-white text-gray-900">
-        {children}
-      </body>
+      <body className="bg-white text-gray-900">{children}</body>
     </html>
   );
 }
